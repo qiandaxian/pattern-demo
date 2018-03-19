f13a9797e920900c29304ebbb5b5fab0d5834c55
qianbaohua <>
2018-03-19 16:43:01 +0800

    单例模式DEMO，懒汉式单例，调用时初始化。

--- src/pattern/singleton/ApplicationEngine.java
+++ src/pattern/singleton/ApplicationEngine.java
@@ -1,14 +1,25 @@
 package pattern.singleton;
 
 public class ApplicationEngine {
-    private ApplicationEngine applicationEngine = null;
+    private static ApplicationEngine applicationEngine = null;
     //私有的构造方法
-    private ApplicationEngine(){}
+    private ApplicationEngine(){
+        System.out.println("引擎初始化操作！");
+    }
 
-    public ApplicationEngine getApplicationEngine(){
+    public static ApplicationEngine getApplicationEngine(){
         if(applicationEngine==null){
-            new ApplicationEngine();
+            applicationEngine = new ApplicationEngine();
         }
+        System.out.println("获取引擎实例");
         return applicationEngine;
+    }
+
+    public static void main(String[] args) {
+        ApplicationEngine.getApplicationEngine();
+        ApplicationEngine.getApplicationEngine();
+        ApplicationEngine.getApplicationEngine();
+        ApplicationEngine.getApplicationEngine();
+
     }
 }
